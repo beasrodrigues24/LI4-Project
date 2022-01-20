44e0b4ef14843111fa17b65e1b34cc9650d3c51b
Beatriz Rodrigues <beatrizsilvarodrigues12@gmail.com>
2022-01-20 14:20:56 +0000

    Controllers

---
+++ Geres4U/Geres4U/Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Geres4U.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetPointsOfInterest()
+        {
+            return View();
+        }
+
+        public IActionResult GetSpecificPointOfInterest()
+        {
+            return View();
+        }
+
+        public IActionResult AddPointVisited()
+        {
+            return View();
+        }
+
+        public IActionResult GetPointsVisited()
+        {
+            return View();
+        }
+
+        public IActionResult SugestPointOfInterest()
+        {
+            return View();
+        }
+    }
+}
